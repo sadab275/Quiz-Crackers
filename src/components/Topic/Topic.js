63e27843e8739460b2161caa12c43ac67aa59ee7
Sadab <sadabrahman275@gmail.com>
2022-10-11 01:50:08 +0600

    button id detect

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Topic.css'
 
 const Topic = ({ test }) => {
-    const { name, logo } = test;
+    const { id, name, logo } = test;
     return (
         <div className='card-container'>
 
-            <div class="card">
+            <div className="card">
                 <img src={logo} alt="" />
 
-                <div class="container">
+                <div className="container">
                     <h4><b>{name}</b></h4>
                     <p><small>{test.length}</small></p>
-                    <Button className='btn'>Start Practice</Button>
+                    <Link to={`/topic/${id}`}>
+                        <Button className='btn'>Start Practice</Button>
+                    </Link>
+
                 </div>
             </div>
 
