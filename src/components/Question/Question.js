59a3f7b322f3930b372a1b4581cbfb8f91911fcf
Sadab <sadabrahman275@gmail.com>
2022-10-11 21:41:28 +0600

    toaster added to see correct answer

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Question.css';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ question }) => {
     // console.log(question);
+    const { correctAnswer } = question;
+    console.log(correctAnswer);
+
+    const eyehandler = (correctAnswer) => toast(`${correctAnswer}`, {
+        position
+            : 'top-center'
+    });
+
+
     return (
         <div className='question-container'>
             <div className='question'>
                 <h5>{question.question}</h5>
-                <EyeIcon className="h-2 w-2 text-blue-500 eye" />
+                <EyeIcon onClick={() => eyehandler(correctAnswer)} className=" h-1 w-1text-blue-500 eye" />
             </div>
             <div className='options'>
                 {
@@ -20,7 +31,7 @@ const Question = ({ question }) => {
                     )
                 }
             </div>
-
+            <ToastContainer />
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { Tooltip } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
-import Barchart from '../Barchart/Barchart';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+
 
 const Statistics = () => {
     const datas = useLoaderData();
+    const { data } = datas;
+    console.log(data);
 
     return (
         <div>
             <h4>This is Statistics</h4>
-            {
+            {/* <ResponsiveContainer width="100%" height="100%"> */}
+            <LineChart width={500} height={400} data={data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
+            {/* </ResponsiveContainer> */}
+            {/* {
                 datas.data.map(singledata =>
                     <Barchart singledata={singledata}></Barchart>
                     // <p>{singledata.total}</p>
                 )
-            }
+            } */}
         </div>
     );
 };
