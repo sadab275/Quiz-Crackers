a782c637e0e3d6af8de861fdcd26ab614c6fe954
Sadab <sadabrahman275@gmail.com>
2022-10-11 13:49:31 +0600

    options box added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
       children: [
         {
           path: '/',
+          loader: () => {
+            return fetch("https://openapi.programming-hero.com/api/quiz");
+          },
 
           element: <Topics></Topics>
         },
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import './Question.css';
 
 const Question = ({ question }) => {
     console.log(question);
     return (
-        <div>
+        <div className='question-container'>
             <div className='question'>
-                <h5>This is question{question.question}</h5>
+                <h5>{question.question}</h5>
             </div>
             <div className='options'>
                 {
-                    question.options.map(option => <p>{option}</p>)
+                    question.options.map(option =>
+                        <div className='options'>
+                            <button className='btn'>{option}</button>
+                            {/* <p>{option}</p> */}
+                        </div>
+                    )
                 }
             </div>
 
